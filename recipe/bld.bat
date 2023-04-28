b45e18d5242c51b4dcf661baa7b2cfd563d9671e
Serhii Kupriienko <79282962+skupr-anaconda@users.noreply.github.com>
2023-04-28 16:06:32 +0300

    nitro 2.7.dev6 (#1)
    * Add release 2.7.dev6
    
    * Use ninja on win

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,20 +1,34 @@
+:: cmd
+echo "Building %PKG_NAME%."
+
 mkdir build
 cd build
+if errorlevel 1 exit /b 1
 
 set CXXFLAGS="/D_HAS_AUTO_PTR_ETC=1"
 
-cmake -G "NMake Makefiles" ^
+:: Generate the build files.
+echo "Generating the build files..."
+cmake .. %CMAKE_ARGS% ^
+      -G"Ninja" ^
       -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
       -DCMAKE_BUILD_TYPE:STRING=Release ^
-	  -DBUILD_SHARED_LIBS=ON ^
+	-DBUILD_SHARED_LIBS=ON ^
       %SRC_DIR%
-if errorlevel 1 exit 1
+if errorlevel 1 exit /b 1
 
-nmake
-if errorlevel 1 exit 1
-
-
-nmake install
-if errorlevel 1 exit 1
+:: Build.
+echo "Building..."
+ninja -j%CPU_COUNT%
+if errorlevel 1 exit /b 1
 
 
+:: Install.
+echo "Installing..."
+ninja install
+if errorlevel 1 exit /b 1
+
+
+:: Error free exit.
+echo "Error free exit!"
+exit 0
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-set -e
-set -x
+
+set -ex
+
 mkdir build
 cd build
+
 cmake ${CMAKE_ARGS} -DCMAKE_INSTALL_PREFIX=$PREFIX ${SRC_DIR}
+
 make -j${CPU_COUNT}
+
 make install
