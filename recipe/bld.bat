e6c4302b1d1e037ed96793a338214dd04c0150d0
Serhii Kupriienko <skupriienko@anaconda.com>
2023-04-28 12:42:49 +0300

    Add release 2.7.dev6

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -6,7 +6,7 @@ set CXXFLAGS="/D_HAS_AUTO_PTR_ETC=1"
 cmake -G "NMake Makefiles" ^
       -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
       -DCMAKE_BUILD_TYPE:STRING=Release ^
-	  -DBUILD_SHARED_LIBS=ON ^
+	-DBUILD_SHARED_LIBS=ON ^
       %SRC_DIR%
 if errorlevel 1 exit 1
 
@@ -16,5 +16,3 @@ if errorlevel 1 exit 1
 
 nmake install
 if errorlevel 1 exit 1
-
-
